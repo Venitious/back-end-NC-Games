473db72bb10cc299905c7c8a4d2c2f7d6c06bccc
venitious <george_olid@hotmail.com>
2023-05-09 15:33:04 +0100

    minor change

--- db/controllers.js
+++ db/controllers.js
@@ -1,6 +1,6 @@
 const { fetchCategories } = require("./models")
 
-exports.formatCategories = (request, response, next) => {
+exports.formatCategories = (request, response) => {
     fetchCategories().then((returnedCategories) => {                
         response.status(200).send({categories: returnedCategories})
     })
