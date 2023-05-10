d3b336010d8757dc0ffe8c1d5c10caa2a5af3a16
venitious <george_olid@hotmail.com>
2023-05-10 11:33:37 +0100

    complete Q4

--- db/app.js
+++ db/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const { formatCategories } = require('./controllers');
-const { handleServerErrors } = require('./errorFunc');
+const { formatCategories, formatReviews, formatSingleReview } = require('./controllers');
+const { handleServerErrors, handleCustomErrors, handlePsqlErrors } = require('./errorFunc');
 const app = express(); 
 
 app.get('/api/categories', formatCategories)
+
+app.get('/api/reviews', formatReviews )
+
+app.get('/api/reviews/:review_id', formatSingleReview)
+
+app.use(handleCustomErrors)
+
+app.use(handlePsqlErrors)
 
 app.use(handleServerErrors)
 
