b4576d736602615befacf18f6f19eca131fd70ea
venitious <george_olid@hotmail.com>
2023-05-12 14:48:58 +0100

    minor changes to hosting

--- db/connection.js
+++ db/connection.js
@@ -5,17 +5,18 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
 const config = 
+  ENV === 'production'
+  ? {
+      connectionString: process.env.DATABASE_URL,
+      max: 2,
+    }
+  : {};
 
-ENV === 'production'
-? {
-    connectionString: process.env.DATABASE_URL,
-    max: 2,
-  }
-: {};
 
-module.exports = new Pool();
+module.exports = new Pool(config);
